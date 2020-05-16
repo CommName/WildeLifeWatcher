00a80b4bcdee2e89c8182f983c1780d5e45bda71
CommName <17132081+CommName@users.noreply.github.com>
2020-05-16 22:04:26 +0200

    Downloading and sending images

---
+++ VideoSensors/CommunicationLayer/Communicator.py
@@ -0,0 +1,32 @@
+import cv2
+import json
+import base64
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Communicator(ABC):
+
+    @abstractmethod
+    async def sendMessage(self, image, gpsNCoordinate, gpsYCoordinate):
+        pass
+
+    @abstractmethod
+    async def recvMessage(self, msg):
+        pass
+
+    @abstractmethod
+    async def addHandler(self, obj, command, arguments):
+        pass
+
+    def encodeMessageJSON(self, image, gpsNCoordinate, gpsYCoordinate):
+        _, imdata = cv2.imencode('.JPG', image)
+        imageAsString = base64.b64encode(imdata).decode()
+        jsonData = json.dumps({"CoordinateN": gpsNCoordinate, "CoordinateE": gpsYCoordinate, "image": imageAsString})
+        return jsonData
+
+    def decodeMessageJSON(selfs, jsonData):
+        imageAsString = jsonData["image"]
+        jpg_original = base64.b64decode(imageAsString)
+        jpg_as_np = np.frombuffer(jpg_original,dtype=np.uint8)
+        img = cv2.imdecode(jpg_as_np,flags=1)
+        return img, jsonData["CoordinateN"], jsonData["CoordinateE"]
--- VideoSensors/CommunicationLayer/NATSCommunication.py
+++ VideoSensors/CommunicationLayer/NATSCommunication.py
@@ -1,67 +1,37 @@
 import asyncio
-import base64
 import json
 from nats.aio.client import Client as NATS
 import cv2
-import numpy as np
+from CommunicationLayer import Communicator
 
-from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
-
-class NATSCommunication:
+class NATSCommunication (Communicator):
 
     nc = None
     sid = None
+    sendTopicName = "WildLife.Sensors.Data"
+    recvTopicRequest = "WildLife.Sensors.API"
 
-
+    async def addHandler(self, obj, command, arguments):
+        return obj
 
     async def closeConnection(self):
-        #await self.nc.unsubscribe(self.sid)
         await self.nc.close()
 
     async def connect(self, address=""):
         self.nc = NATS()
         await self.nc.connect()
-        self.sid = await self.nc.subscribe("test", cb=self.recvMessage)
+        self.sid = await self.nc.subscribe(self.recvTopicRequest, cb=self.recvMessage)
 
     async def sendMessage(self, image, gpsNCoordinate, gpsYCoordinate):
-        _, imdata = cv2.imencode('.JPG', image)
-        imageAsString = base64.b64encode(imdata).decode()
-        jsonData = json.dumps({"CoordinateN" : gpsNCoordinate, "CoordinateY" : gpsYCoordinate, "image":imageAsString})
-        await self.nc.publish("test", jsonData.encode())
+        jsonData = super().encodeMessageJSON()
+        await self.nc.publish(self.sendTopicName, jsonData.encode())
 
 
     async def recvMessage(self, msg):
         subject = msg.subject
         reply = msg.reply
         data = json.loads(msg.data.decode())
-
-
         #await self.nc.publish(reply, b'I can help')
 
 
 
-
-async def run(loop):
-    test = NATSCommunication()
-    await test.connect()
-
-    testSlika = cv2.imread("test.jpg")
-    await test.sendMessage(testSlika,5.0,5.0)
-
-    await asyncio.sleep(5,loop)
-    await test.closeConnection()
-
-    cv2.waitKey()
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(loop))
-    loop.close()
-
-
-    """"" imageAsString = data["image"]
-        jpg_original = base64.b64decode(imageAsString)
-        jpg_as_np = np.frombuffer(jpg_original,dtype=np.uint8)
-        img = cv2.imdecode(jpg_as_np,flags=1)
-        cv2.imshow("test2.jpg",img)
-    """""
