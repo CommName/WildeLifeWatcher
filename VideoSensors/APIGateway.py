189ec6fe0426da00727b8e7bf8f04fcdad12f729
CommName <17132081+CommName@users.noreply.github.com>
2020-05-21 22:49:12 +0200

    API gateway

---
+++ VideoSensors/APIGateway.py
@@ -0,0 +1,28 @@
+import cherrypy
+import cv2
+import numpy as np
+import base64
+import asyncio
+
+@cherrypy.expose
+class Gateway(object):
+
+    Logic = None;
+
+    def __init__(self, logic):
+        print('construct')
+        self.Logic = logic
+
+    @cherrypy.tools.accept(media = "text/plain")
+    def GET(self):
+        return self.Logic.communciator.encodeMessageJSON(self.Logic.lastFrame, self.Logic.coordinateN, self.Logic.coordinateE)
+
+    def POST(self, coordinateN, coordinateE, image):
+        jpg_original = base64.b64decode(image)
+        jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+        img = cv2.imdecode(jpg_as_np, flags=1)
+        self.Logic.coordinateE = coordinateE
+        self.Logic.coordinateN = coordinateN
+        self.Logic.lastFrame = img
+        await self.Logic.communciator.sendMessage(img,coordinateN,coordinateE)
+        return
--- VideoSensors/main.py
+++ VideoSensors/main.py
@@ -2,32 +2,34 @@ from CommunicationLayer import NATSCommunication
 import asyncio
 import argparse
 import os
+import APIGateway
+import cherrypy
+import threading
 
 from Sensors import CSVSensor
-
 
 class Logic:
     lastFrame = None
     coordinateN = 0
     coordinateE = 0
     frameRate = 1
+    sensor = None
+    communciator = None
+
 
     async def run(self, loop):
-        sensor = self.getSensor()
+        self.sensor = self.getSensor()
         #communicator
-        communciator = NATSCommunication.NATSCommunication()
-        communciator.logic = self
-        await communciator.connect(os.getenv("NATSaddress"))
+        self.communciator = NATSCommunication.NATSCommunication()
+        self.communciator.logic = self
+        await self.communciator.connect(os.getenv("NATSaddress"))
 
-        self.lastFrame = sensor.getFrame()
+        self.lastFrame = self.sensor.getFrame()
         while not (self.lastFrame is None):
-            await communciator.sendMessage(self.lastFrame,self.coordinateN,self.coordinateE)
+            await self.communciator.sendMessage(self.lastFrame,self.coordinateN,self.coordinateE)
 
             await asyncio.sleep(1//self.frameRate)
-            self.lastFrame = sensor.getFrame()
-
-
-
+            self.lastFrame = self.sensor.getFrame()
 
     async def getCommunicator(self):
         communciator = NATSCommunication.NATSCommunication()
@@ -41,6 +43,10 @@ class Logic:
         return sensor
 
 
+def asyncoThreading(loop, logic):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(logic.run(loop))
+
 #Main program
 logic = Logic()
 print(os.environ)
@@ -49,5 +55,19 @@ logic.coordinateN = float(os.getenv("NorthCoordiante"))
 logic.frameRate = int(os.getenv("FramesPerSecond"))
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(logic.run(loop))
-loop.close()
+sensorThread = threading.Thread(target=asyncoThreading, args=(loop,logic,))
+sensorThread.start()
+
+API = APIGateway.Gateway(logic)
+conf = {
+    '/': {
+        'request.dispatch' : cherrypy.dispatch.MethodDispatcher(),
+        'tools.response_headers.on' : True,
+        'tools.response_headers.headers' : [('Content-Type', 'text/plain')],
+    }
+}
+
+
+cherrypy.quickstart(API,'/',conf)
+
+
